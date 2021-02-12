1ea831bfef816c4b2dce0351a8b43c62a151210e
Yanjaa Wintersoul <yanjaa@yanjaas-mbp.attlocal.net>
2021-02-12 13:07:47 -0600

    Testing server.js configurations

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const app = express();
 // const DIST_DIR = path.join(__dirname, '/dist');
 // const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
+
 app.use(express.static('index-page'));
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
 
-});
+// });
